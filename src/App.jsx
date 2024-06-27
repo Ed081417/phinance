767597c2a189e6795aa6ddc72e71cbbc8d7a4bf9
Ed081417 <eduardotumimbo.dev@gmail.com>
2024-06-27 11:26:35 +0800

    Installed TailwindCSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import { useState } from 'react'
+import './index.css'
 
 function App() {
   return (
     <>
       <p>
         <h1>Phinance</h1>
+
+        <h1 className="text-3xl font-bold underline">
+          Hello world!
+        </h1>
       </p>
     </>
   )
